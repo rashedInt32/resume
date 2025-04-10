df48cdb1aebb75296ceba1f0d5150c75ae79fdfa
rashed parvez <parvez08eee@gmail.com>
2025-04-10 17:31:42 +0600

    working on auth

--- packages/auth/src/useAuth.ts
+++ packages/auth/src/useAuth.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 import { ActionState, validatedAction } from "./middleware";
 const signInSchema = z.object({
-  email: z.string().email({message: "Enter valid email"}),
-  password: z.string()
+  email: z.string().email({ message: "Enter valid email" }),
+  password: z.string(),
 });
 
-export async function signIn() {
-  return validatedAction(signInSchema, ())
-}
+export const signIn = validatedAction(signInSchema, async (data, formData) => {
+  console.log("called sigIn");
+  console.log(data, formData);
+});
 
 export async function signUp(state: ActionState, formData: FormData) {
   console.log(formData);
