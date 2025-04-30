426e73f2c811c12c03b7118c8937dbb21564d4ab
rashed parvez <parvez08eee@gmail.com>
2025-04-30 16:39:26 +0600

    creates api package for trpc server

--- packages/auth/src/session.ts
+++ packages/auth/src/session.ts
@@ -49,6 +49,7 @@ export async function setSession(user: NewUser) {
     user: { id: user.id! },
     expires: expiresInOneDay.toISOString(),
   };
+
   const encryptedSession = await signToken(session);
   (await cookies()).set("session", encryptedSession, {
     expires: expiresInOneDay,
