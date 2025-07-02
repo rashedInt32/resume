b461180ffaf8e492b5bcea2926583d8beadeecd9
rashed parvez <parvez08eee@gmail.com>
2025-07-02 17:47:22 +0600

    Login register feature

--- packages/trpc/src/router/auth.ts
+++ packages/trpc/src/router/auth.ts
@@ -1,11 +1,38 @@
+import { db } from "@resume/db";
 import { protectedProcedure, publicProcedure } from "../trpc";
 import { router } from "../trpc";
 import { z } from "zod";
+import { usersTable } from "@resume/db/schema";
+import { eq } from "drizzle-orm";
+import { comparePasswords } from "@resume/auth/session";
 
 export const authRouter = router({
   login: publicProcedure
     .input(z.object({ email: z.string(), password: z.string() }))
-    .mutation(async ({ input }) => {}),
+    .mutation(async ({ input }) => {
+      const user = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, input.email))
+        .limit(1);
+
+      if (user.length < 0) {
+        return { error: "Email not found." };
+      }
+
+      const isPasswordMatched = await comparePasswords(
+        input.password,
+        user[0]?.password as string,
+      );
+      if (!isPasswordMatched) {
+        return { error: "Wrong password" };
+      }
+
+      return {
+        username: user[0]?.username,
+        email: user[0]?.email,
+      };
+    }),
   signUp: publicProcedure
     .input(
       z.object({
